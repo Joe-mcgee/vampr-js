79fcbedea530a5a6838c47b9d221be20ed99b4a5
Taylor <thulsmans.2994@me.com>
2018-02-14 20:24:43 +0000

    added functionality to vampr

--- vampire.js
+++ vampire.js
@@ -1,3 +1,35 @@
+function climb(vampire, rank) {
+      if (vampire.creator == null) {
+        return rank
+      } else {
+        rank += 1
+        return climb(vampire.creator, rank)
+      }
+    }
+
+function ancestorMatch(older, younger) {
+  const baseYounger = younger;
+  console.log(0, baseYounger.name)
+  return ancestorMatchLogic(older, younger, baseYounger)
+
+}
+
+function ancestorMatchLogic(older, younger, base) {
+  if (older == younger) {
+    console.log(1, older.name)
+    return older
+  } else {
+    if (younger.creator == null) {
+      console.log(2, older.name, younger.name, base.name)
+      return ancestorMatchLogic(older.creator, base, base);
+    }
+    else {
+      console.log(3, older.name, younger.name, base.name)
+      return ancestorMatchLogic(older, younger.creator, base)
+    }
+  }
+}
+
 class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
@@ -10,22 +42,31 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    return this.offspring.length;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    return climb(this, 0)
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    let thisVamp = climb(this, 0);
+    let otherVamp = climb(vampire, 0);
+    if (thisVamp < otherVamp) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -36,9 +77,17 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let thisIsYounger = this.isMoreSeniorThan(vampire) ? false: true;
+    if (thisIsYounger) {
+      return ancestorMatch(vampire, this)
+    } else {
+      return ancestorMatch(this, vampire)
+    }
 
   }
+
 }
+
 
 module.exports = Vampire;
 
